6dba55db3a51375cea7dddb06c8a0991a07fbb9b
Patrycja <patrycja.skowronska@op.pl>
2023-04-15 21:16:14 +0200

    Dodanie zadania 1.6

--- solo-work/solo_1.py
+++ solo-work/solo_1.py
@@ -46,3 +46,14 @@ wynik = pow(liczba, potega)
 # oczekiwany rezultat:
 # Wynik wynosi: 81
 print("Wynik wynosi: "+str(wynik))
+
+# zadanie 1.6
+
+# policz ilosc nawiasow ( w danym ciagu znakow
+
+ciag_znakow = "edbw(hdakqas(skqskahb))adwndwb(wgwidn()dsqwhjdw)"
+liczba_nawiasow_otwierajacych = ciag_znakow.count("(")
+
+# oczekiwany rezultat:
+# Liczba nawiasow otwierajacych wynosi: 4
+print("Liczba nawiasow otwierajacych wynosi: " + str(liczba_nawiasow_otwierajacych))
