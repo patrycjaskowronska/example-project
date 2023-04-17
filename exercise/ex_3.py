1114ed08bcce06ecdd43891f300da8ba9af5a7c0
Kamil Wrzos <kamilwrzos999@gmail.com>
2023-04-17 12:35:37 +0200

    Kwadrat_i_rownoleglobok

--- exercise/ex_3.py
+++ exercise/ex_3.py
@@ -11,9 +11,10 @@ def trojkat(bok_a, bok_b, bok_c, wysokosc_a):
 
 
 # kwadrat, prostokat dla studenta 1
-def kwadrat(bok):
-    # TODO
-    return 0, 0
+def kwadrat(bok_a):
+    obwod = 4 * bok_a
+    pole = bok_a * bok_a
+    return obwod, pole
 
 
 def prostokat(bok_a, bok_b):
@@ -22,8 +23,9 @@ def prostokat(bok_a, bok_b):
 
 # rownoleglobok i romb dla studenta 2
 def rownoleglobok(bok_a, bok_b, wysokosc_a):
-    # TODO
-    return 0, 0
+    obwod = 2 * bok_a + 2 * bok_b
+    pole = bok_a * wysokosc_a
+    return obwod, pole
 
 def romb(bok, wysokosc):
     # TODO
@@ -41,9 +43,9 @@ def kolo(promien):
 
 
 # assert trojkat(10, 15, 16, 8) == (41, 40)
-# assert kwadrat(20) == (80, 400)
+assert kwadrat(20) == (80, 400)
 # assert prostokat(12, 10) == (44, 120)
-# assert rownoleglobok(6, 5, 2) == (22, 12)
+assert rownoleglobok(6, 5, 2) == (22, 12)
 # assert romb(10, 5) == (40, 50)
 # assert trapez(10, 15, 7, 14, 2) == (45, 25)
 # TODO na koniec (na wyraźne polecenie prowadzącego)! 
