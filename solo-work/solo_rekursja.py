1e7782379768bf1a1eec51b6a382840d328c80e3
Patrycja <patrycja.skowronska@op.pl>
2023-05-22 12:45:37 +0200

    Factorial function

--- solo-work/solo_rekursja.py
+++ solo-work/solo_rekursja.py
@@ -1,7 +1,4 @@
-# funkcja sumująca listę
-# jeśli lista jest pusta -> suma wynosi 0
-
-# funkcja sum list
+# funkcja sumująca elementy list
 # is list empty -> 0
 # is list not empty -> list[0] + sum[reszta]
 
@@ -13,3 +10,15 @@ def sum(list):
         
 list_1 = [3,2,7]
 print(sum(list_1))
+
+# silnia
+# is liczba 0 -> 1
+# is liczba not 0 -> liczba * silnia(liczba-1)
+
+def silnia(liczba):
+    if liczba == 0:
+        return 1
+    else:
+        return liczba * silnia(liczba-1)
+    
+print(silnia(5))
